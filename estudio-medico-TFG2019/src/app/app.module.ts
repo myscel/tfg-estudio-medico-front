6a67a0b7bbd4d7a443a10e8ade9de9d50e749c4e
myscel <edugon04@ucm.es>
2019-11-07 11:43:53 +0100

    first version

--- estudio-medico-TFG2019/src/app/app.module.ts
+++ estudio-medico-TFG2019/src/app/app.module.ts
@@ -13,16 +13,17 @@ import { SubjectsAdminComponent } from './components/admin/subjects-admin/subjec
 import { FormComponent } from './components/form/form.component';
 import { ProfileComponent } from './components/researcher/profile/profile.component';
 import { EditResearcherAdminComponent } from './components/admin/edit-researcher-admin/edit-researcher-admin.component';
+import { GuardServiceService } from './services/guard/guard-service.service';
 
 const appRoutes : Routes = [ 
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'admin/researchers', component: HomeAdminComponent },
-  { path: 'admin/subjects', component: SubjectsAdminComponent },
-  { path: 'admin/researchers/edit/:id', component: EditResearcherAdminComponent},
-  { path: 'researcher', component: HomeResearcherComponent },
-  { path: 'researcher/subjectForm/:idSubject/:appointment', component: FormComponent },
-  { path: 'researcher/profile/:id', component: ProfileComponent }
+  { path: 'admin/researchers', component: HomeAdminComponent, canActivate: [GuardServiceService] },
+  { path: 'admin/subjects', component: SubjectsAdminComponent, canActivate: [GuardServiceService] },
+  { path: 'admin/researchers/edit/:id', component: EditResearcherAdminComponent, canActivate: [GuardServiceService]},
+  { path: 'researcher', component: HomeResearcherComponent, canActivate: [GuardServiceService] },
+  { path: 'researcher/subjectForm/:idSubject/:appointment', component: FormComponent, canActivate: [GuardServiceService] },
+  { path: 'researcher/profile/:id', component: ProfileComponent, canActivate: [GuardServiceService] }
 ];
 
 @NgModule({
@@ -43,7 +44,7 @@ const appRoutes : Routes = [
     FormsModule,
     RouterModule.forRoot(appRoutes) 
   ],
-  providers: [],
+  providers: [GuardServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ estudio-medico-TFG2019/src/app/services/guard/guard-service.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardServiceService implements CanActivate {
+
+  constructor(private router: Router) { }
+
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean{;
+    let userLogged: User = JSON.parse(localStorage.getItem("userLogged"));
+  
+    if(userLogged === null || userLogged.token === null || userLogged.token === ""){
+      localStorage.removeItem('userLogged');
+      this.router.navigate(['/login']);
+    }
+
+    return true;
+  }
+
+  
+}
