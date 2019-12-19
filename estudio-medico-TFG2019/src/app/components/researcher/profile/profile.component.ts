35f4b6adee4d125232670d071e52ba85eeb8738e
myscel <edugon04@ucm.es>
2019-12-19 19:02:52 +0100

    Profile researcher implemented

--- estudio-medico-TFG2019/src/app/components/researcher/profile/profile.component.ts
+++ estudio-medico-TFG2019/src/app/components/researcher/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/User';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { UserServiceService } from 'src/app/services/userService.service';
 
 @Component({
   selector: 'app-profile',
@@ -13,13 +14,26 @@ export class ProfileComponent implements OnInit {
   userLogged: User;
 
   constructor(private router: Router,
-    private http: HttpClient) { }
+    private http: HttpClient,
+    private userService: UserServiceService) { }
 
   ngOnInit() {
     this.userLogged = JSON.parse(localStorage.getItem("userLogged"));
   }
 
   doHome(){
-    this.router.navigate(['/researcher']);
+    this.router.navigate(['/researcher/' + this.userLogged.id]);
   }
+
+  doProfile(){
+    this.router.navigate(['/researcher/profile/' + this.userLogged.id]);
+  }
+
+  doLogOut(){
+    this.userService.logOutResearcherAndAdmin().subscribe(responseData =>{
+      localStorage.removeItem('userLogged');
+      this.router.navigate(['/login']);
+    });
+  }
+  
 }
