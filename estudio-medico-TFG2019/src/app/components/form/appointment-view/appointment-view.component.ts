94b240d19eca89ec927cf9aaf1e9dd1b47a8f274
Pache <serpache@ucm.es>
2020-01-07 13:22:14 +0100

    formView done

--- estudio-medico-TFG2019/src/app/components/form/appointment-view/appointment-view.component.ts
+++ estudio-medico-TFG2019/src/app/components/form/appointment-view/appointment-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from 'src/app/services/userService.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/User';
 
 @Component({
@@ -12,8 +12,52 @@ export class AppointmentViewComponent implements OnInit {
 
   userLogged: User;
 
+  //Datos de la cita
+  idSubject: string;
+  appointmentDate: string;
+
+  //Variables principales
+  vitamin: string;
+  hbA1c: string;
+  season: string;
+
+  //Variables sociodemográficas
+  gender: string;
+  studyLevel: string;
+  birthDate: string;
+  economicLevel: string;
+
+  //Hábitos de vida
+  tobacco: string;
+  alcoholRisk: string;
+  sunExposure: string;
+  spfCream: string;
+  spfGrade: string;
+  exercise: string;
+
+  //Variables clínicas
+  DM2: string;
+  glucose: string;
+  imc: string;
+  obesity: string;
+  tas: string;
+  tad: string;
+  arterialHypertension: string;
+  cholesterol: string;
+  ldl: string;
+  hdl: string;
+  tg: string;
+  dyslipidemia: string;
+  creatinine: string;
+  glomerular: string;
+  kidneyInsufficiency: string;
+  fototype: string;
+  diabetesTreatment: string;
+  vitaminDSupplementation: string;
+
   constructor(
     private router: Router,
+    private activatedRoute: ActivatedRoute,
     private userService: UserServiceService
   ) { }
 
@@ -21,20 +65,21 @@ export class AppointmentViewComponent implements OnInit {
 
     this.userLogged = JSON.parse(localStorage.getItem("userLogged"));
 
+    this.idSubject = this.activatedRoute.snapshot.params.idSubject;
   }
 
-  doLogOut(){
-    this.userService.logOutResearcherAndAdmin().subscribe(responseData =>{
+  doLogOut() {
+    this.userService.logOutResearcherAndAdmin().subscribe(responseData => {
       localStorage.removeItem('userLogged');
       this.router.navigate(['/login']);
     });
   }
 
-  doProfile(){
+  doProfile() {
     this.router.navigate(['/researcher/profile/' + this.userLogged.id]);
   }
 
-  doHome(){
+  doHome() {
     this.router.navigate(['/researcher/' + this.userLogged.id]);
   }
 
